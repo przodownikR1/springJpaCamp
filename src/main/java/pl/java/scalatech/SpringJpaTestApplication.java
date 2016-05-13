c9c8c6331d1725e53748c3654946a9c28d2e6544
Slawomir Borowiec <przodownik@tlen.pl>
2016-05-13 15:02:34 +0200

    init

---
+++ src/main/java/pl/java/scalatech/SpringJpaTestApplication.java
@@ -0,0 +1,24 @@
+package pl.java.scalatech;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class SpringJpaTestApplication implements CommandLineRunner{
+
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaTestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
