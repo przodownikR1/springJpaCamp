9106b409083e3fa0a76301609516c8188e46d778
Slawomir Borowiec <przodownik@tlen.pl>
2016-05-15 15:12:58 +0200

    added simple object to test

---
+++ src/main/java/pl/java/scalatech/domain/common/Address.java
@@ -0,0 +1,15 @@
+package pl.java.scalatech.domain.common;
+
+//zrobic z tego wbudowana
+public class Address {
+
+
+    private String street;
+
+
+
+    private  String city;
+
+
+
+}
---
+++ src/main/java/pl/java/scalatech/domain/simple/Person.java
@@ -0,0 +1,5 @@
+package pl.java.scalatech.domain.simple;
+
+public class Person {
+
+}
