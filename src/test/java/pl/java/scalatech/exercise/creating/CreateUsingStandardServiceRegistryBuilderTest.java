e446cc4604a0a89c8d742a0ca29772853641136a
Slawomir Borowiec <przodownik@tlen.pl>
2016-06-18 13:14:26 +0200

    session builder test

--- src/test/java/pl/java/scalatech/exercise/creating/CreateUsingStandardServiceRegistryBuilderTest.java
+++ src/test/java/pl/java/scalatech/exercise/creating/CreateUsingStandardServiceRegistryBuilderTest.java
@@ -1,9 +1,12 @@
 package pl.java.scalatech.exercise.creating;
 
+
 import org.junit.Test;
 
 public class CreateUsingStandardServiceRegistryBuilderTest {
-
+    
+    //AnnotationConfiguration,  ServiceRegistryBuilder  deprecated from hibernate 4
+    
     @Test
     public void shouldCreateSession(){
         
