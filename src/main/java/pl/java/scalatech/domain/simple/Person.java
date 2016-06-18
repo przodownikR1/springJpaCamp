202a8f0bc0ca3ef63ff62fb33e59887c7fcea643
Slawomir Borowiec <przodownik@tlen.pl>
2016-06-18 14:20:09 +0200

    update strategy added

--- src/main/java/pl/java/scalatech/domain/simple/Person.java
+++ src/main/java/pl/java/scalatech/domain/simple/Person.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Person {
     
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     
     private String name;
