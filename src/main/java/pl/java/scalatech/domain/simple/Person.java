c6ceadcd4723c4a2da76a226d78ffc315f415010
Slawomir Borowiec <przodownik@tlen.pl>
2016-06-18 13:09:14 +0200

    sample Person entity added

--- src/main/java/pl/java/scalatech/domain/simple/Person.java
+++ src/main/java/pl/java/scalatech/domain/simple/Person.java
@@ -1,5 +1,19 @@
 package pl.java.scalatech.domain.simple;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
 public class Person {
+    
+    @Id
+    private Long id;
+    
+    private String name;
 
 }
---
+++ src/main/java/pl/java/scalatech/repository/simple/PersonRepo.java
@@ -0,0 +1,9 @@
+package pl.java.scalatech.repository.simple;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.java.scalatech.domain.simple.Person;
+
+public interface PersonRepo extends JpaRepository<Person, Long>{
+
+}
