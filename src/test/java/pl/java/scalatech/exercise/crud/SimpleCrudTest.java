ad6ea7b0781c08cec3faa0abea4dc7e45274c692
Slawomir Borowiec <przodownik@tlen.pl>
2016-06-19 19:38:52 +0200

    crud exerise

---
+++ src/test/java/pl/java/scalatech/exercise/crud/SimpleCrudTest.java
@@ -0,0 +1,30 @@
+package pl.java.scalatech.exercise.crud;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+
+public class SimpleCrudTest {
+    //inject or create inline
+    private SessionFactory sf;
+    @Test
+    public void shouldCreateEntity(){
+       // Session session = sf.openSession();
+    }
+    @Test
+    public void shouldUpdateEntity(){
+        
+    }
+    @Test
+    public void shouldFindByIdEntity(){
+        
+    }
+    @Test
+    public void shouldFindByNameEntity(){
+        
+    }
+    @Test
+    public void shoulDeleteEntity(){
+        
+    }
+    
+}
